ae3a92dc7f83e492f57a5f1099efd1367dd692f9
fjgruenewald <61251684+fj-gruenewald@users.noreply.github.com>
2021-02-11 15:21:58 +0100

    Creation of the Project and first UI Designs

---
+++ app/src/main/java/com/example/eazyffmpeg/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.eazyffmpeg
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.LinearLayout
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+    //OnClick Events for the Menu Items
+    fun videoCreationClick(view:View)
+    {
+        val intent = Intent(this, activity_creation::class.java)
+        startActivity(intent)
+    }
+    fun videoCuttingClick(view:View)
+    {
+        val intent = Intent(this, activity_cutting::class.java)
+        startActivity(intent)
+    }
+    fun videoConvertClick(view:View)
+    {
+        val intent = Intent(this, activity_converter::class.java)
+        startActivity(intent)
+    }
+    fun videoTextAndTitleClick(view:View)
+    {
+        val intent = Intent(this, activity_text::class.java)
+        startActivity(intent)
+    }
+    fun soundClick(view:View)
+    {
+        val intent = Intent(this, activity_sound::class.java)
+        startActivity(intent)
+    }
+    fun settingsClick(view:View)
+    {
+        val intent = Intent(this, activity_settings::class.java)
+        startActivity(intent)
+    }
+
+}
---
+++ app/src/main/java/com/example/eazyffmpeg/activity_settings.kt
@@ -0,0 +1,11 @@
+package com.example.eazyffmpeg
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_settings : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
