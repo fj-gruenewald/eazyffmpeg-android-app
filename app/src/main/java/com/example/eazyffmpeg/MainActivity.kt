95938a642e1752618dd03d69733f2120eff6891a
fjgruenewald <61251684+fj-gruenewald@users.noreply.github.com>
2021-02-18 12:25:35 +0100

    added the FFmpeg Library
    added the FFmpeg Library from WritingMinds and made some Design changes to the Activities.

--- app/src/main/java/com/example/eazyffmpeg/MainActivity.kt
+++ app/src/main/java/com/example/eazyffmpeg/MainActivity.kt
@@ -5,12 +5,44 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.LinearLayout
+import android.widget.Toast
+import com.github.hiteshsondhi88.libffmpeg.FFmpeg
+import com.github.hiteshsondhi88.libffmpeg.FFmpegLoadBinaryResponseHandler
+import com.github.hiteshsondhi88.libffmpeg.exceptions.FFmpegNotSupportedException
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        try {
+            FFmpeg.getInstance(this).loadBinary(object : FFmpegLoadBinaryResponseHandler {
+                override fun onFailure() {
+                    val toto = Toast.makeText(this@MainActivity," Failed ", Toast.LENGTH_SHORT)
+                    toto.show()
+                }
+
+                override fun onSuccess() {
+                    val toto = Toast.makeText(this@MainActivity," Sucess ", Toast.LENGTH_SHORT)
+                    toto.show()
+                }
+
+                override fun onStart() {
+                    val toto = Toast.makeText(this@MainActivity," Start ", Toast.LENGTH_SHORT)
+                    toto.show()
+                }
+
+                override fun onFinish() {
+                    val toto = Toast.makeText(this@MainActivity," Finish", Toast.LENGTH_SHORT)
+                    toto.show()
+                }
+            })
+        } catch (e: FFmpegNotSupportedException) {
+            e.printStackTrace()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
 
     }
 
--- app/src/main/java/com/example/eazyffmpeg/activity_converter.kt
+++ app/src/main/java/com/example/eazyffmpeg/activity_converter.kt
@@ -2,10 +2,41 @@ package com.example.eazyffmpeg
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
 
 class activity_converter : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_converter)
+
+        //Fill the Codec Spinner
+        //Make the Variables
+        val spinner = findViewById<Spinner>(R.id.codec_spinner)
+        val codecs = resources.getStringArray(R.array.codecs)
+
+        //Check if Spinner is empty ==> give him some strings
+        if (spinner != null) {
+            val adapter = ArrayAdapter(this,
+                    android.R.layout.simple_spinner_item, codecs)
+            spinner.adapter = adapter
+
+            //When something gets selected ==> print what got selected
+            spinner.onItemSelectedListener = object :
+                    AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(parent: AdapterView<*>,
+                                            view: View, position: Int, id: Long) {
+                    Toast.makeText(this@activity_converter,
+                            getString(R.string.selected_item) + " " +
+                                    "" + codecs[position], Toast.LENGTH_SHORT).show()
+                }
+                override fun onNothingSelected(parent: AdapterView<*>) {
+                    // write code to perform some action
+                }
+            }
+        }
     }
 }
